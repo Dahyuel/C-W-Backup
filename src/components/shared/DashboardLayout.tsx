eee6c03f046aff5ca5168a0520e48686c55b31cf
Dahyuel <u2864279@uel.ac.uk>
2025-09-27 13:30:53 +0300

    Updated DashboardLayout.tsx

--- src/components/shared/DashboardLayout.tsx
+++ src/components/shared/DashboardLayout.tsx
@@ -23,7 +23,7 @@ import {
 } from 'lucide-react';
 import { supabase } from '../../lib/supabase';
 import QRCode from 'qrcode';
-import Leaderboard from '/shared/Leaderboard';
+import Leaderboard from '../Leaderboard';
 
 interface DashboardLayoutProps {
   children: ReactNode;
