08a394d0a55e61fc3dc3e1d9e36fc826c7ff8285
ahmeddahy8 <ronaldoae2005@gmail.com>
2025-10-10 21:49:02 +0300

    Updated constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -23,6 +23,7 @@ export const FACULTIES = [
   'Faculty of Alsun',
   'Faculty of Business Administration',
   'Faculty of Applied Arts',
+    'Faculty of Girls',
   'Other'
 ];
 export const ENUM_VALUES = {
